50fe3df960f42013632a5a97ffeef0e26726ad48
nkepner <nkepner@gmail.com>
2025-01-29 17:39:15 -0500

    removed NewURLParser and UnifiedTopology

--- backend/song_service/index.js
+++ backend/song_service/index.js
@@ -13,8 +13,6 @@ const PORT = process.env.PORT;
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Blog Service connected to MongoDB");
