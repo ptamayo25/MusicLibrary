6e4d50910bbbcefbca56dee2fc4bf7bb1e5c812c
nkepner <nkepner@gmail.com>
2025-01-29 17:19:38 -0500

    added index files and moved gitignore

---
+++ backend/api_gateway/index.js
@@ -0,0 +1,47 @@
+require("dotenv").config(); // Load environment variables from .env file
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API Gateway is running.");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`API Gateway is running on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express"); // Import the Express.js framework for building the API Gateway
+// //const cors = require("cors"); // Middleware to enable Cross-Origin Resource Sharing (CORS)
+// //const bodyParser = require("body-parser"); // Middleware to parse incoming JSON request bodies
+// //const gatewayRoutes = require("./routes/gatewayRoutes"); // Import API Gateway routes
+// //const logger = require("./gatewayLogger/logger"); // Import the custom logger middleware to log requests and responses
+// //const loggerMiddleware = require("./gatewayLogger/logger").middleware;
+
+
+// // Initialize the Express.js application
+// const app = express();
+
+// // Set the port from environment variables, or use 5000 as a default
+// const PORT = process.env.PORT || 5005;
+
+// app.use("/api", gatewayRoutes);
+
+// // Start the server
+// app.listen(PORT, () => {
+//   logger.info({ message: `API Gateway is running on port ${PORT}` });
+//   console.log(`API Gateway is running on port ${PORT}`);
+// });
+
---
+++ backend/auth_service/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config(); // Load environment variables from .env file
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Auth Service is running.");
+});
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Auth Service running on port ${PORT}`);
+});
+
+
+
+
+
+
