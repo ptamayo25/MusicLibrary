3624c9ce4563f01b00c8141ccf6ac8271556e0f3
Jacqueline Xu <xujacque@gmail.com>
2025-01-30 11:58:56 -0500

    Setting up router, react-router pkg added (make sure to npm install)

---
+++ frontend/src/router/index.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./app";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
