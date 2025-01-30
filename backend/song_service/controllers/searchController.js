7e8de9e0a80cfddd0a99c92d2801f483741012c4
Priscilla <priscillatamayoeit@gmail.com>
2025-01-30 06:26:19 -0800

    picked up JQ comment for condition if no songs found. also added case insensitivity to all searches

--- backend/song_service/controllers/searchController.js
+++ backend/song_service/controllers/searchController.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Song = require("../models/song"); //update if Nancy changes name of schema
 
+//stretch goal: PJT to add fuzzy search capability (for misspelled words)
+
 //function returning all songs with words in title
 exports.getSongsBySearch = async (req, res) => {
   try {
@@ -8,9 +10,11 @@ exports.getSongsBySearch = async (req, res) => {
     const songsByTitle = await Song.find({
       title: { $regex: words, $options: "i" },
     }).exec();
-    const songByComposer = await Song.find({ composer: composer }).exec();
+    const songByComposer = await Song.find({
+      composer: { $regex: words, $options: "i" },
+    }).exec();
     const songsByLyrics = await Song.find({
-      lyrics: { $regex: wordsInLyrics, $options: "i" },
+      lyrics: { $regex: words, $options: "i" },
     }).exec();
     const songsByKeywords = await Song.find({
       keywords: { $regex: words, $options: "i" },
@@ -21,6 +25,9 @@ exports.getSongsBySearch = async (req, res) => {
       ...songsByLyrics,
       ...songsByKeywords,
     ];
+    if (songs.length === 0) {
+      res.status(404).json({ message: "No songs found" });
+    }
     res.json(sortResults(songs, sortType));
   } catch (error) {
     res.status(500).json({ message: error.message });
