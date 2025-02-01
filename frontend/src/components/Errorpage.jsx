0afb48ec23b174839bd5805fbb8a652660f2fc68
Jacqueline Xu <xujacque@gmail.com>
2025-02-01 11:27:49 -0500

    Error page with a samoyed :D

--- frontend/src/components/Errorpage.jsx
+++ frontend/src/components/Errorpage.jsx
@@ -1,5 +1,23 @@
+import SamoyedError from "../assets/samoyederror.jpg";
+import "./styles/bgcolors.css";
+
 const Errorpage = () => {
-  return <div>Errorpage</div>;
+  return (
+    <>
+      <div className="errorpage">
+        <h1> Page not found. </h1>
+        <img
+          className="Samoyed"
+          src={SamoyedError}
+          alt="Derpy samoyed dog with error message"
+        />
+        <h2> Please try again!!! 🐶</h2>
+        <button>
+          <a href="/">Back to Homepage</a>
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Errorpage;
