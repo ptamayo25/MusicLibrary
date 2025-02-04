6c0d89c47cd03c837c061b18df5880fea2a0ace6
nkepner <nkepner@gmail.com>
2025-02-04 09:27:22 -0500

    removed AddForm homepage testing

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,13 +1,10 @@
 import Navigation from "../components/Navigation/Navigation";
 import SearchInput from "./SearchInput/SearchInput";
-import AddForm from "../components/AddForm/AddForm";
 
 const Homepage = () => {
   return (
     <div>
-      <AddForm />
       <Navigation />
-
       <SearchInput />
     </div>
   );
