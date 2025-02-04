e3cf5bc4e45406bd0fe260947d0610ec3983a4a0
nkepner <nkepner@gmail.com>
2025-02-04 09:12:48 -0500

    AddForm is actually Adding Song, tested in homepage

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,9 +1,11 @@
 import Navigation from "../components/Navigation/Navigation";
 import SearchInput from "./SearchInput/SearchInput";
+import AddForm from "../components/AddForm/AddForm";
 
 const Homepage = () => {
   return (
     <div>
+      <AddForm />
       <Navigation />
 
       <SearchInput />
