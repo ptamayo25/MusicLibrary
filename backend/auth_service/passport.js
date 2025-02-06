6bf7749b99b5fe899869d27ad5c92214f3d16208
Priscilla <priscillatamayoeit@gmail.com>
2025-02-06 07:28:03 -0800

    added medium article walkthrough used

--- backend/auth_service/passport.js
+++ backend/auth_service/passport.js
@@ -1,21 +1,27 @@
-const passport =require("passport")
-const GoogleStrategy = require('passport-google-oauth20').Strategy; //breadcrumb
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy; //breadcrumb
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
+//Medium Article walkthrough on passport.js
+//https://medium.com/nerd-for-tech/google-oauth2-0-authentication-using-node-js-and-passportjs-1a77f42b1111
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-        done(null, user);
+passport.deserializeUser(function (user, done) {
+  done(null, user);
 });
 
-passport.use(new GoogleStrategy({
-        clientID:process.env.GOOGLE_CLIENT_ID,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:4001/google/callback",
-        passReqToCallback: true
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:4001/google/callback",
+      passReqToCallback: true,
     },
-    function(request, accessToken, refreshToken, profile, done) {
-            return done(null, profile);
+    function (request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
     }
-));
+  )
+);
