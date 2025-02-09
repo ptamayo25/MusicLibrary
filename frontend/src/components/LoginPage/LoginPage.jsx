bc43e0dc099c7d379306885063e646a0c61cca9b
Priscilla <priscillatamayoeit@gmail.com>
2025-02-09 14:21:23 -0800

    trying to make sure the latest is pushed

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -49,6 +49,7 @@ const Login = () => {
               required
             />
           </div> */}
+          
           <button type="submit">Login or Register with Google</button>
         </form>
       </div>
