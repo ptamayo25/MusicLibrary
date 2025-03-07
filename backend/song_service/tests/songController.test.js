b55a56c1646c793450f6406ab622a4de2813ee88
Priscilla <priscillatamayoeit@gmail.com>
2025-03-07 14:38:28 -0800

    added all failure tests for song controller CRUD functions

--- backend/song_service/tests/songController.test.js
+++ backend/song_service/tests/songController.test.js
@@ -25,6 +25,7 @@ describe("Song Controller tests", () => {
     await mongoose.connection.close();
   });
 
+  //Successful tests
   test("should create a new song", async () => {
     const req = {
       body: {
@@ -165,5 +166,173 @@ describe("Song Controller tests", () => {
       })
     );
     await expect(Song.findOne({ title: "Test Song" })).resolves.toBeNull();
+  });
+
+  //Failure tests
+  test("should not create a new song without required field of title", async () => {
+    const req = {
+      body: {
+        composer: "Test Composer",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await createSong(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: "Title and composer are required",
+      })
+    );
+  });
+
+  test("should not create a new song without required field of composer", async () => {
+    const req = {
+      body: {
+        title: "Test Song",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await createSong(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: "Title and composer are required",
+      })
+    );
+  });
+
+  test("should not get a song by id is not a valid ObjectID type", async () => {
+    const req = {
+      params: {
+        id: "123456789012",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await getSongById(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: "Invalid ID format",
+      })
+    );
+  });
+
+  test("should not get a song by id if song not found", async () => {
+    const song = new Song({
+      title: "Test Song",
+      composer: "Test Composer",
+    });
+    await song.save();
+    const songToDelete = await Song.findOne({ title: "Test Song" });
+    const req = {
+      params: {
+        id: songToDelete._id,
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await Song.deleteOne({ _id: songToDelete._id });
+
+    await getSongById(req, res);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: "Song not found.",
+      })
+    );
+  });
+
+  test("should not update a song if song not found", async () => {
+    const song = new Song({
+      title: "Test Song",
+      composer: "Test Composer",
+    });
+    await song.save();
+    const songToDelete = await Song.findOne({ title: "Test Song" });
+    const req = {
+      params: {
+        id: songToDelete._id,
+      },
+      body: {
+        composer: "Updated Song Composer",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await Song.deleteOne({ _id: songToDelete._id });
+
+    await updateSong(req, res);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: "Song not found",
+      })
+    );
+  });
+
+  test("should not delete song if id is not a valid ObjectID type", async () => {
+    const req = {
+      params: {
+        id: "123456789012",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await deleteSong(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: "Invalid song ID",
+      })
+    );
+  });
+
+  test("should not delete a song if song not found", async () => {
+    const song = new Song({
+      title: "Test Song",
+      composer: "Test Composer",
+    });
+    await song.save();
+    const songToDelete = await Song.findOne({ title: "Test Song" });
+    const req = {
+      params: {
+        id: songToDelete._id,
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    await Song.deleteOne({ _id: songToDelete._id });
+
+    await deleteSong(req, res);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: "Song not found",
+      })
+    );
   });
 });
